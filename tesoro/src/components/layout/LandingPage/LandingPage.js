01abeacce4ddd07b84d01023d627ce3664035f9b
Fernando Doddoli <a00827038@itesm.mx>
2022-10-08 22:57:01 -0500

    project setup

---
+++ tesoro/src/components/layout/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LandingPage.css';
+import { Link } from 'react-router-dom';
+
+const LandingPage = () => {
+
+    return(
+       <div className="">
+            <h1>hello</h1>
+       </div>
+    );
+}
+
+export default LandingPage;
